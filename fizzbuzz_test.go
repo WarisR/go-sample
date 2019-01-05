98086748b31e02d64c5b770485e09f4087dc33a7
warisr <waris.r@linecorp.com>
2019-01-05 11:32:00 +0700

    add Say 1 test

---
+++ fizzbuzz_test.go
@@ -0,0 +1,14 @@
+package fizzbuzz_test
+
+import (
+	"testing"
+	"github.com/WarisR/fizzbuzz"
+)
+
+func TestFizzBuzzShouldSayOne(t *testing.T) {
+	result := fizzbuzz.Say(1)
+	expected := "1"
+	if result != expected {
+		t.Errorf("it should say %q but get %q", expected, result)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/WarisR/fizzbuzz
