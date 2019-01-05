ec9fc963f73aa97f7f3eefd471e4b38babc9573b
warisr <waris.r@linecorp.com>
2019-01-05 11:44:05 +0700

    Say Buzz when input is 5

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/WarisR/fizzbuzz"
 )
 
-func TestFizzBuzzShouldSayOne(t *testing.T) {
+func TestFizzBuzzShouldSay1(t *testing.T) {
 	result := fizzbuzz.Say(1)
 	expected := "1"
 	if result != expected {
@@ -13,7 +13,7 @@ func TestFizzBuzzShouldSayOne(t *testing.T) {
 	}
 }
 
-func TestFizzBuzzShouldSayTwo(t *testing.T) {
+func TestFizzBuzzShouldSay2(t *testing.T) {
 	result := fizzbuzz.Say(2)
 	expected := "2"
 	if result != expected {
@@ -21,9 +21,25 @@ func TestFizzBuzzShouldSayTwo(t *testing.T) {
 	}
 }
 
-func TestFizzBuzzShouldSayFizz(t *testing.T) {
+func TestFizzBuzzShouldSayFizzWhenInputIs3(t *testing.T) {
 	result := fizzbuzz.Say(3)
 	expected := "Fizz"
+	if result != expected {
+		t.Errorf("it should say %q but get %q", expected, result)
+	}
+}
+
+func TestFizzBuzzShouldSay4(t *testing.T) {
+	result := fizzbuzz.Say(4)
+	expected := "4"
+	if result != expected {
+		t.Errorf("it should say %q but get %q", expected, result)
+	}
+}
+
+func TestFizzBuzzShouldSayBuzzWhenInputIs5(t *testing.T) {
+	result := fizzbuzz.Say(5)
+	expected := "Buzz"
 	if result != expected {
 		t.Errorf("it should say %q but get %q", expected, result)
 	}
